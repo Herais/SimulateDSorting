4a5bbe67f00267fc47695cc00e94b5a40b9ca998
Herais <48284363+Herais@users.noreply.github.com>
2023-05-26 05:12:24 +0800

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,5 +1,5 @@
-pip install flowkit ==1.0.1
-pip install bokeh ==2.4.3
-pip install pandas ==1.5.3
+pip install flowkit>=1.0.1
+pip install bokeh>=2.4.3
+pip install pandas>=1.5.3
 pip install matplotlib
 pip install pandas
