02017801979c244cb08f55bede107a7687881ecc
Herais <veexu@qq.com>
2023-05-29 00:00:27 -0700

    updated dsorter

--- module/Dsort.py
+++ module/Dsort.py
@@ -287,12 +287,12 @@ class DropletSorter(object):
             axis=1
             )
         
-        ret['df']['sum_'.format(colname_f1)] = ret['df'].apply(
+        ret['df']['sum_{}'.format(colname_f1)] = ret['df'].apply(
             lambda x: sum(x['values_padded'][:x['num_cells_at_saturation_in_droplet']]),
             axis=1)
         
         #plot histogram
-        cols_to_plot=['sum_'.format(colname_f1), 'sum_'.format(colname_f1), 'size_droplet', 'num_cells_encapsulated', 'num_cells_at_saturation_in_droplet']
+        cols_to_plot=['sum_{}'.format(colname_f1), 'sum_{}'.format(colname_f1), 'size_droplet', 'num_cells_encapsulated', 'num_cells_at_saturation_in_droplet']
         logicle_xform = fk.transforms.LogicleTransform('logicle', param_t=262144, param_w=0.5, param_m=4.5, param_a=0)
         xform_funcs = [None, logicle_xform, None, None, None]
         ls_bins = [200, 200, None, None, None]
