03eac66665b2ce69c8f186db49f359d882ee11c9
Herais <48284363+Herais@users.noreply.github.com>
2023-07-06 10:57:35 -0700

    updated PCR

--- module/PCR.py
+++ module/PCR.py
@@ -507,6 +507,156 @@ class PCR(object):
         return ret
 
 
+    @staticmethod
+    def multiplex_dd_pcr(
+            amplicons,
+            nums_template_amplicon,
+            V_i_bulk_ul=200,
+            n_droplets=10000,
+            determine_n_droplets_from_V_i=True,
+            efficiency_bulk_to_droplet=0.8,
+            size_droplet_um=20,
+            size_type='diameter',
+            func_droplet_size=None, #np.random.normal,
+            scale_droplet_size=0,
+            C_i_dna_ng_per_ul=10,
+            C_i_dnTP_mM=0.2,
+            C_i_primer_F_uM=0.5,
+            C_i_primer_R_uM=0.5,
+            C_i_polymerase_U=1, # to be updated
+            library_type='dsDNA',
+            fragment_size_avg=550,
+            num_pcr_cycles=40,
+            t_denature_intial=95,
+            t_denature_cycle=30,
+            t_anneal_cycle=62,
+            efficiencies_dntp=None,
+            efficiencies_dntp_type='square',
+            efficiencies_access=None,
+            efficiencies_denature=None,
+            efficiencies_anneal=None,
+            efficiencies_k=None,
+        ):
+
+        ret = {}
+
+
+        # droplet size(s)
+        V_droplet_um3 = Droplet.calculate_volume(size=size_droplet_um, size_type=size_type)
+        V_droplet_ul = Droplet.convert_um3_to_ul(V_droplet_um3)
+
+        # V_total
+        if determine_n_droplets_from_V_i:
+            n_droplets = int(V_i_bulk_ul*efficiency_bulk_to_droplet/V_droplet_ul)
+        V_total_ul = V_droplet_ul * n_droplets
+        ret['n_droplets'] = n_droplets
+        ret['Ws_dropid'] = None
+
+        if func_droplet_size:
+            sizes_droplet = np.random.normal(size_droplet_um, scale_droplet_size, n_droplets)
+            f = lambda x: Droplet.calculate_volume(x)
+            Vs_droplet_um3 = f(sizes_droplet)
+            f = lambda x: Droplet.convert_um3_to_ul(x)
+            Vs_droplet_ul = f(Vs_droplet_um3)
+            V_total_ul = sum(Vs_droplet_ul)
+            ret['Vs_droplet_ul'] = Vs_droplet_ul
+            Ws_dropid = np.array(Vs_droplet_ul) / V_total_ul
+            ret['Ws_dropid'] = Ws_dropid         
+        ret['V_total_ul'] = V_total_ul
+
+        # amplicons
+        assert len(amplicons) > 0, 'please provide at least one amplicon sequences'
+        num_unique_amplicons = len(amplicons)
+        lengths_amplicon = np.array(list(map(len, amplicons)))
+        amplicon = np.array(amplicons)
+
+        nums_template_amplicon = np.array(nums_template_amplicon)
+        nums_template_amplicon = nums_template_amplicon * V_total_ul / V_i_bulk_ul
+        nums_template_amplicon = nums_template_amplicon.astype('int')
+        ret['nums_template_amplicon'] = nums_template_amplicon
+
+        # dna
+        mass_dna_ng = C_i_dna_ng_per_ul * V_total_ul
+        if func_droplet_size:
+            masses_dna_ng = mass_dna_ng * Vs_droplet_ul / V_total_ul
+
+        # compartmentalize
+        ret_bulk2drop = PCR.compartmentalize_bulk_to_drops(
+                            nums_template_amplicon=nums_template_amplicon,
+                            n_droplets=n_droplets,
+                            V_total_ul=V_total_ul,
+                            Ws_dropids=ret['Ws_dropid'],
+                        )
+        num_droplets_nonempty = len(ret_bulk2drop['dropid_to_aid2count'])
+        num_droplets_empty = n_droplets - num_droplets_nonempty
+        ret['num_droplets_nonempty'] = num_droplets_nonempty
+        ret['num_droplets_empty'] = num_droplets_empty
+
+        if func_droplet_size:
+            sizes_droplet_nonemtpy = 'Vs_total_ul'[list(ret_bulk2drop['dropid_to_aid2count'].keys())]
+        else:
+            sizes_droplet_nonempty = np.array([V_droplet_ul]*len(ret_bulk2drop['dropid_to_aid2count']))
+        
+        combos_in_non_empty_droplets =  [tuple(k for k, v in d.items() for i in range(v)) 
+                                for d in ret_bulk2drop['dropid_to_aid2count'].values()]
+        combo2count = Counter(combos_in_non_empty_droplets)
+        ret['combo2count'] = combo2count
+
+        # run PCR
+        combo2ret_pcr = {}
+        combo2df = {}
+        ls = []
+        for k, v in combo2count.items():
+            amplicons_droplet = [amplicons[i] for i in k]
+            amplicon2count = Counter(amplicons_droplet)
+            amplicons_droplet = list(amplicon2count.keys())
+            nums_template_initial_droplet = list(amplicon2count.values())
+            n_droplets_combo = v
+
+            ret_pcr_droplet = PCR.run_droplet_PCR(
+                                    amplicons=amplicons_droplet,
+                                    nums_template_initial=nums_template_initial_droplet,
+                                    size_droplet_um=size_droplet_um,
+                                    num_pcr_cycles=num_pcr_cycles,
+                                    dnTP_mM=C_i_dnTP_mM,
+                                    dna_ng_per_ul=C_i_dna_ng_per_ul,
+                                    fragment_size_avg=fragment_size_avg, #bp
+                                    primer_F_uM = C_i_primer_F_uM,
+                                    primer_R_uM = C_i_primer_R_uM,
+                                    t_denature_intial=t_denature_intial,
+                                    t_denature_cycle=t_denature_cycle,
+                                    t_anneal_cycle=t_anneal_cycle,
+                                    efficiencies_dntp=efficiencies_dntp,
+                                    efficiencies_dntp_type=efficiencies_dntp_type,
+                                    efficiencies_access=None,
+                                    efficiencies_denature=None,
+                                    efficiencies_anneal=None,
+                                    efficiencies_k=efficiencies_k,
+                                    library_type='dsDNA',
+                                    use_tensor=False,
+                                )
+            combo2ret_pcr[k] = ret_pcr_droplet
+
+            df_a_droplet = pd.DataFrame.from_dict(ret_pcr_droplet['ncycle2ret'], orient='index')
+
+            dfcombo = df_a_droplet[['amplicons']].copy()
+            dfcombo['nums_template_at_start_of_cycle'] = df_a_droplet['nums_template_at_start_of_cycle'].apply(lambda x: np.array(x)*n_droplets_combo)
+            dfcombo['{}_amplicon2counter'.format(k)] = dfcombo.apply(lambda x: Counter(dict(zip(x['amplicons'], x['nums_template_at_start_of_cycle']))), axis=1)
+            ls.append(dfcombo['{}_amplicon2counter'.format(k)])
+
+            combo2df[k] = dfcombo
+        
+        df_pooled = pd.concat(ls, axis=1)
+        df_pooled['all'] = df_pooled.apply(lambda x: sum(x, Counter()), axis=1)
+        df_pooled['V_total_ul'] = V_total_ul
+        ret['df_pooled'] = df_pooled
+
+        ret['combo2ret_pcr'] = combo2ret_pcr
+        ret['combo2df'] = combo2df
+
+        return ret
+
+
 class Polymerase(object):
  
     def __init__(self):
